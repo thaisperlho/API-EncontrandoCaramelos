cf70f033ccbaeb092e27d45ce65069e24bee8030
thaisperlho <carvalho.thais0301@gmail.com>
2022-11-11 17:57:11 -0300

    update OngController.java and OngTestController.java

--- projetoec/src/main/java/br/edu/fatec/projetoec/controller/OngController.java
+++ projetoec/src/main/java/br/edu/fatec/projetoec/controller/OngController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,25 +20,46 @@ import br.edu.fatec.projetoec.service.OngService;
 @RestController
 @RequestMapping("/ongs")
 public class OngController {
-	
+		
 	@Autowired
 	private OngService ongService;
+		
+		
+	@GetMapping	
+	public ResponseEntity<List<Ong>> findALL(){
+		
+		List<Ong> list = ongService.findALL();
+		return ResponseEntity.ok(list);
+	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Ong> find(@PathVariable(name = "id") Integer id){
+		Optional<Ong> ong = this.ongService.findById(id);
+		if (ong.isPresent()) {
+			return ResponseEntity.ok(ong.get());
+		} else {
+			return ResponseEntity.notFound().build();		
+			}
+	}
 	
-@GetMapping	
-public ResponseEntity<List<Ong>> findALL(){
+	@PutMapping("/{id}")
+	public ResponseEntity<Ong> edit(@PathVariable(name = "id") Integer id, @RequestBody Ong ong){
+		  Optional<Ong> ongActual = this.ongService.findById(id);
+		  if(ongActual.isPresent()) {
+			  ongActual.get().setNome(ong.getNome());
+			  ongActual.get().setTelefone(ong.getTelefone());
+			  ongActual.get().setSite(ong.getSite());
+			  return ResponseEntity.ok(this.ongService.save(ongActual.get()));
+		  } else {
+			  return ResponseEntity.notFound().build();
+		  }
+	 }
 	
-	List<Ong> list = ongService.findALL();
-	return ResponseEntity.ok(list);
-}
-
-@GetMapping("/{id}")
-public ResponseEntity<Ong> find(@PathVariable(name = "id") Integer id){
-	Optional<Ong> ong = this.ongService.findById(id);
-	if (ong.isPresent()) {
-		return ResponseEntity.ok(ong.get());
-	} else {
-		return ResponseEntity.notFound().build();		}
-}
-
+	  @PostMapping
+	  public ResponseEntity<Ong> creat(@RequestBody Ong ong) {
+		  Ong ongCreated = ongService.save(ong);
+		  
+		  return ResponseEntity.status(HttpStatus.CREATED).body(ongCreated);
+	  }
+	
 }
--- projetoec/src/main/java/br/edu/fatec/projetoec/controller/OngTestController.java
+++ projetoec/src/main/java/br/edu/fatec/projetoec/controller/OngTestController.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,8 +37,16 @@ public class OngTestController {
 		if (ongtest.isPresent()) {
 			return ResponseEntity.ok(ongtest.get());
 		} else {
-			return ResponseEntity.notFound().build();		}
+			return ResponseEntity.notFound().build();		
+			}
 	}
 	
+	
+	  @PostMapping
+	  public ResponseEntity<OngTest> creat(@RequestBody OngTest ongTest) {
+		  OngTest ongCreated = ongTestService.save(ongTest);
+		  
+		  return ResponseEntity.status(HttpStatus.CREATED).body(ongCreated);
+	  }
 
 }
