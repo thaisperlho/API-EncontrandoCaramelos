eed87f0c8b1bc5e4dae55702ab42cea0cdee5c79
thaisperlho <carvalho.thais0301@gmail.com>
2022-11-11 13:50:55 -0300

    update Pets.java

--- projetoec/src/main/java/br/edu/fatec/projetoec/model/Pets.java
+++ projetoec/src/main/java/br/edu/fatec/projetoec/model/Pets.java
@@ -1,6 +1,5 @@
 package br.edu.fatec.projetoec.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
