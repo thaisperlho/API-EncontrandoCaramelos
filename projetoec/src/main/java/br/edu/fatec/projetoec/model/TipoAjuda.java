11ed64b8f6936fe5a02345465faddafc252ccae5
thaisperlho <carvalho.thais0301@gmail.com>
2022-11-11 13:17:26 -0300

    Update model

--- projetoec/src/main/java/br/edu/fatec/projetoec/model/TipoAjuda.java
+++ projetoec/src/main/java/br/edu/fatec/projetoec/model/TipoAjuda.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,8 +16,6 @@ public class TipoAjuda {
 	private Integer id;
 	private String tipoAjuda;
 	
-	@OneToOne(mappedBy = "tipoAjuda")
-	private Ong ong;
 	
 	public TipoAjuda() {
 		
