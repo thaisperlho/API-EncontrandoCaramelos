f6e6c1336c8b6ed6421b4f31a6648525a328da17
thaisperlho <carvalho.thais0301@gmail.com>
2022-11-12 10:07:02 -0300

    update ResponsavelOng.java and Usuario.java

--- projetoec/src/main/java/br/edu/fatec/projetoec/model/ResponsavelOng.java
+++ projetoec/src/main/java/br/edu/fatec/projetoec/model/ResponsavelOng.java
@@ -2,6 +2,8 @@ package br.edu.fatec.projetoec.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -11,6 +13,7 @@ import javax.persistence.Table;
 public class ResponsavelOng {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private Integer id;
 	private String nome;
