a7cd89fb6c5402c82e289f499a6dae6ec9a5b5f8
unknown <larissaribeiro03@hotmail.com>
2022-11-18 21:25:38 -0300

    Implementação de Delete

--- projetoec/src/main/java/br/edu/fatec/projetoec/controller/EndOngController.java
+++ projetoec/src/main/java/br/edu/fatec/projetoec/controller/EndOngController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,17 @@ public class EndOngController {
 		  EnderecoOng endOngCreated = endOngService.save(ong);
 		  
 		  return ResponseEntity.status(HttpStatus.CREATED).body(endOngCreated);
+	  }
+	  
+	  @DeleteMapping("/{id}")
+	  public ResponseEntity<?> delete(@PathVariable(name = "id") Integer id){
+		  Optional<EnderecoOng> endOngActual = this.endOngService.findById(id);
+		  if(endOngActual.isPresent()) {
+			  endOngService.delete(id);
+			  return ResponseEntity.ok(null);
+		  } else {
+			  return ResponseEntity.notFound().build();
+		  }
 	  }
 
 }
--- projetoec/src/main/java/br/edu/fatec/projetoec/controller/PetController.java
+++ projetoec/src/main/java/br/edu/fatec/projetoec/controller/PetController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.fatec.projetoec.model.Ong;
 import br.edu.fatec.projetoec.model.Pet;
 import br.edu.fatec.projetoec.service.PetService;
 
@@ -58,6 +60,17 @@ public class PetController {
 		  Pet petCreated = petService.save(pet);
 		  
 		  return ResponseEntity.status(HttpStatus.CREATED).body(petCreated);
+	  }
+	  
+	  @DeleteMapping("/{id}")
+	  public ResponseEntity<?> delete(@PathVariable(name = "id") Integer id){
+		  Optional<Pet> petActual = this.petService.findById(id);
+		  if(petActual.isPresent()) {
+			  petService.delete(id);
+			  return ResponseEntity.ok(null);
+		  } else {
+			  return ResponseEntity.notFound().build();
+		  }
 	  }
 
 }
